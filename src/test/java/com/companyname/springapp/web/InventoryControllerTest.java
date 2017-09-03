af4c87ead5f79bcb7a079d04f2b0762c4668efd3
lytves <lytves@gmail.com>
2017-09-03 21:56:30 +0200

    rewrite method testHandleRequestView

---
+++ src/test/java/com/companyname/springapp/web/InventoryControllerTest.java
@@ -0,0 +1,32 @@
+package com.companyname.springapp.web;
+
+import static org.junit.Assert.*;
+
+import com.companyname.springapp.service.SimpleProductManager;
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+public class InventoryControllerTest {
+
+    @Test
+    public void testHandleRequestView() throws Exception{
+
+        InventoryController controller = new InventoryController();
+        controller.setProductManager(new SimpleProductManager());
+
+        ModelAndView modelAndView = controller.handleRequest(null, null);
+
+        assertEquals("hello", modelAndView.getViewName());
+        assertNotNull(modelAndView.getModel());
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> modelMap = (Map<String, Object>) modelAndView.getModel().get("model");
+        String nowValue = (String) modelMap.get("now");
+
+        assertNotNull(nowValue);
+
+    }
+
+}
