36e7a0687c2e5290b70a1d6fb13310426cd5d6ac
lytves <lytves@gmail.com>
2017-09-03 21:02:38 +0200

    implementado test method for increasePrice

--- src/test/java/com/companyname/springapp/service/SimpleProductManagerTest.java
+++ src/test/java/com/companyname/springapp/service/SimpleProductManagerTest.java
@@ -23,6 +23,8 @@ public class SimpleProductManagerTest {
     private static String TABLE_DESCRIPTION = "Table";
     private static Double TABLE_PRICE = new Double(150.10);
 
+    private static int POSITIVE_PRICE_INCREASE = 10;
+
     @Before
     public void setUp() throws Exception {
         productManager = new SimpleProductManager();
@@ -63,4 +65,41 @@ public class SimpleProductManagerTest {
         assertEquals(TABLE_DESCRIPTION, product.getDescription());
         assertEquals(TABLE_PRICE, product.getPrice());
     }
+
+    @Test
+    public void testIncreasePriceWithNullListOfProducts() {
+        try {
+            productManager = new SimpleProductManager();
+            productManager.increasePrice(POSITIVE_PRICE_INCREASE);
+        }
+        catch(NullPointerException ex) {
+            fail("Products list is null.");
+        }
+    }
+
+    @Test
+    public void testIncreasePriceWithEmptyListOfProducts() {
+        try {
+            productManager = new SimpleProductManager();
+            productManager.setProducts(new ArrayList<Product>());
+            productManager.increasePrice(POSITIVE_PRICE_INCREASE);
+        } catch (Exception ex) {
+            fail("Products list is empty.");
+        }
+
+    }
+    @Test
+    public void testIncreasePriceWithPositivePercentage() {
+        productManager.increasePrice(POSITIVE_PRICE_INCREASE);
+        double expectedChairPriceWithIncrease = 22.55;
+        double expectedTablePriceWithIncrease = 165.11;
+
+        List<Product> products = productManager.getProducts();
+        Product product = products.get(0);
+        assertEquals(expectedChairPriceWithIncrease, product.getPrice(), 0);
+
+        product = products.get(1);
+        assertEquals(expectedTablePriceWithIncrease, product.getPrice(), 0);
+    }
+
 }
